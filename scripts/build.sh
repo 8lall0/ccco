abad0826fa21b67b976cd84f544ad38521807253
8lall0 <savino.liguori@gmail.com>
2025-10-25 17:44:12 +0200

    Initial work on printf

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,5 +5,5 @@ set -e
 (
   cd ..
   c3c build
-  riscv32-unknown-elf-ld -T scripts/linker.ld build/obj/elf-riscv32/ccco.o -o build/ccco.elf
+  riscv32-unknown-elf-ld -T scripts/linker.ld build/obj/elf-riscv32/*.o -o build/ccco.elf
 )
