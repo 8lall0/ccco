76e1f98cb4cf0d56cd608d376fd2b4a6d34ea68a
8lall0 <savino.liguori@gmail.com>
2025-10-27 21:10:43 +0100

    Improved build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
+rm -rf ../build/obj
+
 set -e
 
 (
   cd ..
-  c3c build
-  riscv32-unknown-elf-ld -T scripts/linker.ld build/obj/elf-riscv32/*.o -o build/ccco.elf
+  c3c build --no-entry
+  ld.lld -T scripts/linker.ld -L /opt/riscv/lib/linux -lclang_rt.builtins-riscv32 build/obj/elf-riscv32/*.o -o build/ccco.elf
 )
+  #
