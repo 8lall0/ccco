77437bc630fbc736dc5293c8540bb22cad04b897
8lall0 <savino.liguori@gmail.com>
2025-10-30 13:46:32 +0100

    First working implementation with putchar added, now stdlib printf works!

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,5 +7,8 @@ set -e
 (
   cd ..
   c3c build --no-entry --riscv-cpu=rvimac
-  /opt/riscv/bin/ld.lld -T scripts/linker.ld build/obj/elf-riscv32/*.o -o build/ccco.elf
+  #riscv32-unknown-elf-as -march=rv32imac -mabi=ilp32 -o build/obj/elf-riscv32/errno.o src/asm/errno.S
+  #riscv32-unknown-elf-as -march=rv32imac -mabi=ilp32 -o build/obj/elf-riscv32/sqrt.o src/asm/sqrt.S
+  #riscv32-unknown-elf-as -march=rv32imac -mabi=ilp32 -o build/obj/elf-riscv32/sqrtf.o src/asm/sqrtf.S
+  /home/blallo/riscv/bin/ld.lld -T scripts/linker.ld -L /home/blallo/riscv/lib/linux -lclang_rt.builtins-riscv32 build/obj/elf-riscv32/*.o -o build/ccco.elf
 )
